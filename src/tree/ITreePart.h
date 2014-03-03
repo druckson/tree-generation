b755632138f7590aff08557c23f8aae5bcfeee6a
Dan Erickson <druckson@gmail.com>
2014-03-03 15:05:05 -0800

    fix memory leak

--- src/tree/ITreePart.h
+++ src/tree/ITreePart.h
@@ -2,10 +2,11 @@
 #define ITREEPART_H
 
 class ITreePart {
-    //ITreePart(ITreePart const &);
-    //ITreePart &operator=(ITreePart const &);
+    //ITreePart(ITreePart const &) = delete;
+    //ITreePart &operator=(ITreePart const &) = delete;
 public:
     //ITreePart();
+    virtual ~ITreePart() {}
     virtual ITreePart* grow() = 0;
     virtual void draw() = 0;
     virtual void print() = 0;
