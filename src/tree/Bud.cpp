069abe571b3aa453cfa832bf215d03f1f0c6b539
Dan Erickson <druckson@gmail.com>
2014-03-04 08:11:03 -0800

    add stochastic bud fates

--- src/tree/Bud.cpp
+++ src/tree/Bud.cpp
@@ -8,21 +8,38 @@ using namespace std;
 Bud::Bud() {}
 
 Bud::Bud(BudFate fate) {
-    this->fate = Branch;
+    this->fate = fate;
 }
 
 ITreePart* Bud::grow() {
-    auto internode = new Internode(); 
-    auto node = new Node(new Bud(), new Bud()); 
-    //node->parent = internode;
-    internode->child.reset(node);
-    //internode->parent = parent;
-    return internode;
+    if (this->fate == Branch) {
+        auto internode = new Internode(); 
+
+        BudFate fate1;
+        if (rand() % 100 < 100) {
+            fate1 = Branch;
+        } else {
+            fate1 = Dormant;
+        }
+
+        BudFate fate2;
+        if (rand() % 100 < 1) {
+            fate2 = Branch;
+        } else {
+            fate2 = Dormant;
+        }
+
+        auto node = new Node(new Bud(fate1), new Bud(fate2)); 
+        //node->parent = internode;
+        internode->child.reset(node);
+        //internode->parent = parent;
+        return internode;
+    } else {
+        return this;
+    }
 }
 
-void Bud::draw() {
-
-}
+void Bud::draw() {}
 
 void Bud::print() {
     cout << "Bud: " << sizeof(*this) << endl;
